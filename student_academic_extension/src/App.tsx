fe2b3fada82e117923142ee7cc695a1844286c79
TomHacker69 <ankitomar.shs@gmail.com>
2026-07-17 18:14:47 +0530

    feat(theme): add system theme detection, time-based scheduling, manual override, and theme preview

--- student_academic_extension/src/App.tsx
+++ student_academic_extension/src/App.tsx
@@ -1,27 +1,30 @@
 import { Routes, Route } from 'react-router'
+import { ThemeProvider } from './app/context/ThemeContext'
 import Layout from './components/Layout'
-import Dashboard from './screens/Dashboard'
-import AttendanceTrends from './screens/AttendanceTrends'
-import Notifications from './screens/Notifications'
-import AssignmentCalendar from './screens/AssignmentCalendar'
-import PersonalizedDashboard from './screens/PersonalizedDashboard'
-import CgpaTracker from './screens/CgpaTracker'
-import Timetable from './screens/Timetable'
-import SemesterComparison from './screens/SemesterComparison'
+import Dashboard from './app/screens/Dashboard'
+import AttendanceTrends from './app/screens/AttendanceTrends'
+import Notifications from './app/screens/Notifications'
+import AssignmentCalendar from './app/screens/AssignmentCalendar'
+import PersonalizedDashboard from './app/screens/PersonalizedDashboard'
+import CgpaTracker from './app/screens/CgpaTracker'
+import Timetable from './app/screens/Timetable'
+import SemesterComparison from './app/screens/SemesterComparison'
 
 export default function App() {
   return (
-    <Layout>
+    <ThemeProvider>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/attendance" element={<AttendanceTrends />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/assignments" element={<AssignmentCalendar />} />
-        <Route path="/dashboard" element={<PersonalizedDashboard />} />
-        <Route path="/cgpa" element={<CgpaTracker />} />
-        <Route path="/timetable" element={<Timetable />} />
-        <Route path="/comparison" element={<SemesterComparison />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/attendance" element={<AttendanceTrends />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/assignments" element={<AssignmentCalendar />} />
+          <Route path="/dashboard" element={<PersonalizedDashboard />} />
+          <Route path="/cgpa" element={<CgpaTracker />} />
+          <Route path="/timetable" element={<Timetable />} />
+          <Route path="/comparison" element={<SemesterComparison />} />
+        </Route>
       </Routes>
-    </Layout>
+    </ThemeProvider>
   )
 }
